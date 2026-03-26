f48dafd85a8ad4b2e9772bbb3a7f7cbe33b933b8
Quentin Macé <quentin.mace@quarksgroup.com>
2026-03-26 10:37:43 +0100

    Upgrade Symfony to version 7.4 anf fix bugs and deprecation

--- src/Controller/Admin/AlbumController.php
+++ src/Controller/Admin/AlbumController.php
@@ -6,34 +6,32 @@ use App\Entity\Album;
 use App\Entity\Media;
 use App\Form\AlbumType;
 use App\Form\MediaType;
+use App\Repository\AlbumRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class AlbumController extends AbstractController
 {
-    /**
-     * @Route("/admin/album", name="admin_album_index")
-     */
-    public function index()
+    #[Route('/admin/album', name: 'admin_album_index')]
+    public function index(AlbumRepository $albumRepository)
     {
-        $albums = $this->getDoctrine()->getRepository(Album::class)->findAll();
+        $albums = $albumRepository->findAll();
 
         return $this->render('admin/album/index.html.twig', ['albums' => $albums]);
     }
 
-    /**
-     * @Route("/admin/album/add", name="admin_album_add")
-     */
-    public function add(Request $request)
+    #[Route('/admin/album/add', name: 'admin_album_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager)
     {
         $album = new Album();
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->persist($album);
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager->persist($album);
+            $entityManager->flush();
 
             return $this->redirectToRoute('admin_album_index');
         }
@@ -41,17 +39,15 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/update/{id}", name="admin_album_update")
-     */
-    public function update(Request $request, int $id)
+    #[Route('/admin/album/update/{id}', name: 'admin_album_update')]
+    public function update(Request $request, int $id, AlbumRepository $albumRepository, EntityManagerInterface $entityManager)
     {
-        $album = $this->getDoctrine()->getRepository(Album::class)->find($id);
+        $album = $albumRepository->find($id);
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager->flush();
 
             return $this->redirectToRoute('admin_album_index');
         }
@@ -59,14 +55,12 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/update.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/delete/{id}", name="admin_album_delete")
-     */
-    public function delete(int $id)
+    #[Route('/admin/album/delete/{id}', name: 'admin_album_delete')]
+    public function delete(int $id, AlbumRepository $albumRepository, EntityManagerInterface $entityManager)
     {
-        $media = $this->getDoctrine()->getRepository(Album::class)->find($id);
-        $this->getDoctrine()->getManager()->remove($media);
-        $this->getDoctrine()->getManager()->flush();
+        $media = $albumRepository->find($id);
+        $entityManager->remove($media);
+        $entityManager->flush();
 
         return $this->redirectToRoute('admin_album_index');
     }
--- src/Controller/Admin/MediaController.php
+++ src/Controller/Admin/MediaController.php
@@ -4,16 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Media;
 use App\Form\MediaType;
+use App\Repository\MediaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MediaController extends AbstractController
 {
-    /**
-     * @Route("/admin/media", name="admin_media_index")
-     */
-    public function index(Request $request)
+    #[Route('/admin/media', name: 'admin_media_index')]
+    public function index(Request $request, MediaRepository $mediaRepository)
     {
         $page = $request->query->getInt('page', 1);
 
@@ -23,13 +23,13 @@ class MediaController extends AbstractController
             $criteria['user'] = $this->getUser();
         }
 
-        $medias = $this->getDoctrine()->getRepository(Media::class)->findBy(
+        $medias = $mediaRepository->findBy(
             $criteria,
             ['id' => 'ASC'],
             25,
             25 * ($page - 1)
         );
-        $total = $this->getDoctrine()->getRepository(Media::class)->count([]);
+        $total = $mediaRepository->count([]);
 
         return $this->render('admin/media/index.html.twig', [
             'medias' => $medias,
@@ -38,10 +38,8 @@ class MediaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/admin/media/add", name="admin_media_add")
-     */
-    public function add(Request $request)
+    #[Route('/admin/media/add', name: 'admin_media_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager)
     {
         $media = new Media();
         $form = $this->createForm(MediaType::class, $media, ['is_admin' => $this->isGranted('ROLE_ADMIN')]);
@@ -53,8 +51,8 @@ class MediaController extends AbstractController
             }
             $media->setPath('uploads/' . md5(uniqid()) . '.' . $media->getFile()->guessExtension());
             $media->getFile()->move('uploads/', $media->getPath());
-            $this->getDoctrine()->getManager()->persist($media);
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager->persist($media);
+            $entityManager->flush();
 
             return $this->redirectToRoute('admin_media_index');
         }
@@ -62,14 +60,12 @@ class MediaController extends AbstractController
         return $this->render('admin/media/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/media/delete/{id}", name="admin_media_delete")
-     */
-    public function delete(int $id)
+    #[Route('/admin/media/delete/{id}', name: 'admin_media_delete')]
+    public function delete(int $id, MediaRepository $mediaRepository, EntityManagerInterface $entityManager)
     {
-        $media = $this->getDoctrine()->getRepository(Media::class)->find($id);
-        $this->getDoctrine()->getManager()->remove($media);
-        $this->getDoctrine()->getManager()->flush();
+        $media = $mediaRepository->find($id);
+        $entityManager->remove($media);
+        $entityManager->flush();
         unlink($media->getPath());
 
         return $this->redirectToRoute('admin_media_index');
